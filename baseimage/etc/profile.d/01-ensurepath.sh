f0ddfbe1b04d75e6aca34e23588b86fd84ca5921
John Muchovej <jmuchovej@users.noreply.github.com>
2023-01-30 11:27:06 -0500

    🚑️ Attempting to enable vers mgr installs
    `nvm` has been repeatedly failing to correctly install, and it appears
    to be due to the fact that `NVM_DIR` is unspecifed (and instead relies
    on `/etc/profile.d/01-ensurepath.sh` to be sourced), but the current
    mechanism for putting `NVM_DIR` into `env` doesn't appear to actually
    work... 😕

--- baseimage/etc/profile.d/01-ensurepath.sh
+++ baseimage/etc/profile.d/01-ensurepath.sh
@@ -1,15 +1,15 @@
-export NVM_DIR="${HOME}/.local/opt/nvm"
-export PYENV_ROOT="${HOME}/.local/opt/pyenv"
+export NVM_DIR="/usr/local/nvm"
+export PYENV_ROOT="/usr/local/pyenv"
 # export JULIAUP_ROOT="${HOME}/.local/opt/juliaup"
-export JULIAUP_ROOT="${HOME}/.julia/bin"
+export JULIAUP_ROOT="/usr/local/juliaup"
 
 PATH="/usr/local/bin:${PATH}"
 PATH="${HOME}/.julia/bin:${PATH}"
-PATH="${HOME}/.local/opt/nvm:${PATH}"
-PATH="${HOME}/.local/opt/pyenv/bin:${PATH}"
-PATH="${HOME}/.local/opt/pyenv/shims:${PATH}"
+PATH="${NVM_DIR}:${PATH}"
+PATH="${PYENV_ROOT}/bin:${PATH}"
+PATH="${PYENV_ROOT}/shims:${PATH}"
 PATH="${HOME}/.local/bin:${PATH}"
 export PATH
 
-eval "$(pyenv init -)"
+[ -d ${PYENV_ROOT} ] && eval "$(pyenv init -)"
 eval "$(starship init $(basename ${SHELL}))"
